7dbe359ddece5c328a9d57e77a053dae08fb6bbf
LaveeshkaMahadea <laveeshka94@gmail.com>
2022-09-07 17:03:30 +1000

    lab submission

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 function addingEventListener() {
+    const input = document.getElementById("button");
+    input.addEventListener('click', clickAlert);
+
 }
+
+//callback function to handle the click event
+function clickAlert() {
+    alert("I was called!");
+}
+
+//call the addEventListener() function to activate the event listener
+addingEventListener();
